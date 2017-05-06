d513400f80599ec071828e223d39708e69596692
Michael <sibozhang@users.noreply.github.com>
2017-05-06 17:21:05 -0500

    Add files via upload

---
+++ app/model.js
@@ -0,0 +1,34 @@
+// Pulls Mongoose dependency for creating schemas
+var mongoose    = require('mongoose');
+var Schema      = mongoose.Schema;
+
+// Creates a User Schema. This will be the basis of how user data is stored in the db
+var UserSchema = new Schema({
+    _id: Number,
+    querywindowId: Number,
+    rank: Number,
+    twitterid: Number,
+    tag: String, // [Long, Lat]
+    lat: Number,
+    lng: Number,
+    htmlverified: String,
+    created_at: {type: Date, default: Date.now},
+    updated_at: {type: Date, default: Date.now}
+});
+
+
+// Sets the created_at parameter equal to the current time
+UserSchema.pre('save', function(next){
+    now = new Date();
+    this.updated_at = now;
+    if(!this.created_at) {
+        this.created_at = now
+    }
+    next();
+});
+
+// Indexes this schema in 2dsphere format (critical for running proximity searches)
+UserSchema.index({location: '2dsphere'});
+
+// Exports the UserSchema for use elsewhere. Sets the MongoDB collection to be used as: "scotch-users"
+module.exports = mongoose.model('NewSchema', UserSchema);
---
+++ app/routes.js
@@ -0,0 +1,41 @@
+// Dependencies
+var mongoose        = require('mongoose');
+var User            = require('./model.js');
+
+
+// Opens App Routes
+module.exports = function(app) {
+
+    // GET Routes
+    // --------------------------------------------------------
+    // Retrieve records for all users in the db
+    app.get('/users', function(req, res){
+
+        // Uses Mongoose schema to run the search (empty conditions)
+        var query = User.find({});
+        query.exec(function(err, users){
+            if(err)
+                res.send(err);
+
+            // If no errors are found, it responds with a JSON of all users
+            res.json(users);
+        });
+    });
+
+    // POST Routes
+    // --------------------------------------------------------
+    // Provides method for saving new users in the db
+    app.post('/users', function(req, res){
+
+        // Creates a new User based on the Mongoose schema and the post bo.dy
+        var newuser = new User(req.body);
+
+        // New User is saved in the db.
+        newuser.save(function(err){
+            if(err)
+                res.send(err);
+
+            // If no errors are found, it responds with a JSON of the new user
+            res.json(req.body);
+        });
+    });
